9f2bc1ad29d6f927fad92ab41a5a9d4b3c3bef01
EmilianoCiurcilo <ciurcilo03@gmail.com>
2023-10-12 00:15:38 -0300

    Entrega final prueba 1

--- js/script.js
+++ js/script.js
@@ -1,194 +1,74 @@
+const contenedorCard = document.querySelector("#contenedorCard")
+const abrirCarrito = document.querySelector("#abrirCarrito")
+const contenedorModal = document.querySelector("#contenedorModal")
 
-class producto {
-    constructor(producto, cantidad) {
-        this.precio = producto.precio;
-        this.nombre = producto.nombre
-        this.cantidad = cantidad;
-        this.precioTotal = producto.precio;
-    }
-    agregarUnidad() {
-        this.cantidad++;
-    }
-    sacarUnidad() {
-        this.cantidad--;
-    }
-    actualizarPrecioTotal() {
-        this.precioTotal = this.precio * this.cantidad;
-    }
-}
-const productos = [
-    {
-    id: "0",
-    nombre: "Almendras", 
-    precio: 1000,
-    categoria: "frutos secos",
-    img:"./imagenes/almendras.jpg"
-    },
-    {
-    id: "1",
-    nombre: "Mani", 
-    precio: 500,
-    categoria: "frutos secos",
-    img:"./imagenes/mani.jpeg"
-    },      
-    {
-    id: "2",
-    nombre: "Nueces", 
-    precio: 700,
-    categoria: "frutos secos",
-    img:"./imagenes/nueces.jpg"
-    },
-    {
-    id : "3",
-    nombre: "Pasas de uva negras", 
-    precio: 200,
-    categoria: "frutas deshidratados",
-    img:"./imagenes/pasas-negras.png"
-    },
-    {
-    id : "4",
-    nombre: "Pasas de uva rubias", 
-    precio: 250,
-    categoria: "frutas deshidratados",
-    img:"./imagenes/pasas-rubias.jpg"
-    },
-    {
-    id : "5",
-    nombre: "Aritos frutales", 
-    precio: 200,
-    categoria: "cereales",
-    img:"./imagenes/aritos-frutales.png"        
-    },
-    {
-    id : "6",
-    nombre: "Almohaditas", 
-    precio: 300,
-    categoria: "cereales",
-    img:"./imagenes/almohaditas.png"       
-    },
-    {
-    id : "7",
-    nombre: "Copos de maiz", 
-    precio: 300,
-    categoria: "cereales",
-    img:"./imagenes/copos-de-maiz.jpg"
-    },
-    ];
+let carrito = []
 
-let carrito = [];
+let contenidoHeader = document.createElement("nav");
+contenidoHeader.innerHTML =
+`<h2 class="titulo"> FrutiFitMix </h2>
+<button id="logoCarrito" class="bi bi-cart-plus-fill"></button>`
 
-function chequearCarritoStorage() {
-    let contenidoStorage = JSON.parse(localStorage.getItem("carritoStorage"));
+abrirCarrito.appendChild(contenidoHeader)
 
-    if (contenidoStorage) {
-        for (const objeto of contenidoStorage) {
-            let producto = new producto(objeto, objeto.cantidad);
-            producto.precioFinal();
+const logoCarrito = document.querySelector("#logoCarrito")
 
-            carrito.push(producto)}
-    }
-    tabla(carrito);
+logoCarrito.addEventListener("click", () => {
+    contenedorModal.innerHTML = ""
+    contenedorModal.style.display = "flex"
+    let modal = document.createElement("div");
+    modal.className = "modal"
+    modal.innerHTML = 
+    `<h2 class="modal-titulo"> TU CARRITO </h2>
+    <button id="modal-x" class="bi bi-x-circle"></button>`
 
-    return carrito;
-}
+// const modalX = document.querySelector("#modal-x")
+    // let cerrarModal = modalX
+    // cerrarModal.addEventListener("click", () => {
+    //     contenedorModal.style.display = "none"
+    // })
 
-const contenedor = document.querySelector("#contenedorCard")
-
-productos.forEach((producto) => {
-    let contenedorProducto = document.createElement("div")
-    contenedorProducto.classList.add("cardProducto")
-    contenedorProducto.style.width ="18rem"
+    contenedorModal.appendChild(contenidoHeader);
+    contenidoHeader.appendChild(modal)
     
-    contenedorProducto.innerHTML =`
-    <div class="card" style="width: 18rem;">
-    <img src="${producto.img}" class="card-img-top">
-    <div class="card-body">
-    <h5 class="card-title">${producto.nombre}</h5>
-    <p class="card-text">$${producto.precio}</p>
-    <a id="agregar${producto.id}" class="btn btn-primary">Agregar al carrito</a>
-    </div>
-    </div>`
+    carrito.forEach((producto) => {
+    let carritoModal = document.createElement("div")
+    carritoModal.className = "carrito-Modal"
+    carritoModal.innerHTML = 
+    `<img src=${producto.img}
+    <h5>${producto.nombre}</h5>
+    <p>$${producto.precio}</p>`,
 
-    contenedor.appendChild(contenedorProducto);
-
-    let boton = document.getElementById(`agregar${producto.id}`)
-    boton.addEventListener("click", () => agregarProducto(producto.id))
+    contenedorModal.appendChild(carritoModal)
+    })
 })
 
-function agregarProducto(idProducto){
-    let productoEnCarrito = carrito.find((producto) => producto.id === idProducto);
+const traerProductos = async () => {
+    const respuesta = await fetch('./productos.json')
+    const productos = await respuesta.json()
 
-    if (productoEnCarrito) {
-        let index = carrito.findIndex((elemento) => elemento.id === productoEnCarrito.id);
-        carrito[index].agregarUnidad();
-    }
-    else{
-        let cantidad = 1;
-        carrito.push(new producto(productos[idProducto], cantidad));
-    }
-    localStorage.setItem("carritoStorage", JSON.stringify(carrito));
-    tabla(carrito);
+    productos.forEach((producto) => {
+        let contenido = document.createElement("div");
+        contenido.className = "card";
+        contenido.innerHTML =
+        `<img src=${producto.img} class="card-img-top">
+        <h5 class="card-title">${producto.nombre}</h5>
+        <p class="card-text">$${producto.precio}</p>
+        <button id="agregar${producto.id}" class="btn btn-primary">Agregar al carrito</button>`
+
+        contenedorCard.appendChild(contenido)
+
+        let agregar = document.querySelector(`#agregar${producto.id}`)
+        
+        agregar.addEventListener("click", () => {
+            carrito.push({
+                id: producto.id,
+                nombre: producto.nombre,
+                precio: producto.precio,
+                categoria: producto.categoria,
+            });
+        });
+    });
 }
-
-function eliminarDelCarrito(id) {
-    let producto = carrito.find((producto) => producto.id === id);
-    let index = carrito.findIndex((elemento) => elemento.id === producto.id);
-    if (producto.cantidad > 1) {
-        carrito[index].sacarUnidad();
-    }
-    else{
-        carrito.splice(index, 1);
-    }
-    localStorage.setItem("carritoStorage", JSON.stringify(carrito));
-    tabla(carrito);
-}
-function eliminarCarrito() {
-    carrito.length = 0;
-    localStorage.removeItem("carritoStorage");
-
-    document.querySelector("#carrito").innerHTML = "";
-    document.querySelector("#acciones").innerHTML = "";
-}
-function obtenerPrecioTotal(carrito) {
-    return carrito.reduce((total, elemento) => total + elemento.precioTotal, 0);
-}
-function tabla(carrito) {
-    let precioTotal = obtenerPrecioTotal(carrito);
-    let contenedor = document.querySelector("#carrito");
-    contenedor.innerHTML = "";
-
-    let tabla = document.createElement("div");
-
-    tabla.innerHTML = `
-        <table id="tablaCarrito" class="table table-striped">
-            <thead>         
-                <tr>
-                    <th>Item</th>
-                    <th>Cantidad</th>
-                    <th>Precio</th>
-                    <th>Accion</th>
-                </tr>
-            </thead>
-
-            <tbody id="bodyTabla">
-            </tbody>
-        </table>
-    `;
-
-    contenedor.appendChild(tabla);
-
-    let bodyTabla = document.getElementById("bodyTabla");
-
-    for (let producto of carrito) {
-        let datos = document.createElement("tr");
-        datos.innerHTML = `
-            <td>${producto.nombre}</td>
-            <td>${producto.cantidad}</td>
-            <td>$${producto.precioTotal}</td>`;
-
-        bodyTabla.appendChild(datos);
-    }
-    let accionesCarrito = document.getElementById("acciones");
-    accionesCarrito.innerHTML = `
-		<h5>PrecioTotal: $${precioTotal}</h5></br>`;
-}
+traerProductos()
+console.log(carrito);
